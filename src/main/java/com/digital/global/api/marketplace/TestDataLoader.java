235ae1f97cb124ccc6fbaaa7c78aef47e76d0aad
Kapila Silwathge <kapila.silwathge@gmail.com>
2020-07-10 05:06:36 +0530

    minor refactoring

--- src/main/java/com/digital/global/api/marketplace/TestDataLoader.java
+++ src/main/java/com/digital/global/api/marketplace/TestDataLoader.java
@@ -7,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.digital.global.api.marketplace.entity.PublisherUser;
-import com.digital.global.api.marketplace.repository.ApiDcoumentRepository;
 import com.digital.global.api.marketplace.repository.PublisherUserRepository;
 
 @Component
@@ -17,17 +16,14 @@ public class TestDataLoader implements ApplicationRunner {
 	private PublisherUserRepository publisherRepo;
 
 	@Autowired
-	private ApiDcoumentRepository apiRepo;
-
-	@Autowired
 	private PasswordEncoder bcryptEncoder;
 
 	public void run(ApplicationArguments args) {
 
 		PublisherUser testUserInDB = publisherRepo.findByUsername("username");
 		if (null == testUserInDB) {
-			PublisherUser publisher = publisherRepo.save(new PublisherUser("username",
-					bcryptEncoder.encode("password"), "kapila@kapila.com", false));
+			publisherRepo.save(new PublisherUser("username", bcryptEncoder.encode("password"),
+					"kapila@kapila.com", false));
 		}
 
 	}
